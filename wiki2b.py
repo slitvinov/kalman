96d6d345422e4682b79bc5522cecd70ece520337
Sergey Litvinov <slitvinov@gmail.com>
2025-03-19 16:50:07 +0100

    yapf -i

--- wiki2b.py
+++ wiki2b.py
@@ -1,29 +1,33 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
 
 rng = np.random.default_rng(seed=12345)
 dim = 2
-dt = 1
 x = np.array([0, 0], dtype=float)
-u = np.array([0, 0], dtype=float)
-F = np.array([[1, dt], [0, 1]], dtype=float)
-B = np.eye(dim)
-Q = np.array([[1 / 4 * dt**4, 1 / 2 * dt**3], [1 / 2 * dt**3, dt**2]],
-             dtype=float)
-R = np.eye(dim)
+F = np.array([[1, 1], [0, 1]], dtype=float)
+
+cQ = np.array([[1 / 2, 0], [1, 0]], dtype=float)
+Q = cQ @ cQ.T
+
+cR = np.eye(dim)
+R = cR @ cR.T
+
 H = np.eye(dim)
 
 xx = np.array([0, 0], dtype=float)
 P = np.eye(dim)
 Trace = []
 
-for t in range(20):
-    x = F @ x + B @ u + scipy.linalg.sqrtm(Q) @ rng.normal(0, 1, dim)
-    z = H @ x + scipy.linalg.sqrtm(R) @ rng.normal(0, 1, dim)
+steps = 20
+traj = []
+for t in range(steps):
+    x = F @ x + cQ @ rng.normal(0, 1, dim)
+    z = H @ x + cR @ rng.normal(0, 1, dim)
+    traj.append((x, z))
 
-    xp = F @ xx + B @ u
+for x, z in traj:
+    xp = F @ xx
     P = F @ P @ np.transpose(F) + Q
 
     y = z - H @ xp
@@ -35,7 +39,6 @@ for t in range(20):
     Trace.append((x, z, xx))
 
 x, z, xx = zip(*Trace)
-
 x0, x1 = zip(*x)
 z0, z1 = zip(*z)
 xx0, xx1 = zip(*xx)
@@ -45,7 +48,7 @@ plt.legend(["state", "observation", "estimate"])
 plt.xlabel("time step")
 plt.ylabel("position")
 plt.gca().xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
-plt.savefig("wiki2b.0.svg")
+plt.savefig("wiki2b.0.png")
 plt.close()
 
 plt.plot(x1, "-b", z1, "o", xx1, "-r")
@@ -53,4 +56,4 @@ plt.xlabel("time step")
 plt.ylabel("velocity")
 plt.gca().xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
 plt.legend(["state", "observation", "estimate"])
-plt.savefig("wiki2b.1.svg")
+plt.savefig("wiki2b.1.png")
